f99ef23a71eb7be25ba0bc55d322920f3f07cea3
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-08-09 17:00:31 -0700

    Update Vampire-numbers.js

--- Vampire-numbers.js
+++ Vampire-numbers.js
@@ -25,3 +25,13 @@ function sortStr(v) {
 }
 
 // or
+
+let vampire_test = function(a, b) {
+  let origDigits = String(a) + String(b),
+      prodDigits = String(a * b);
+
+  origDigits = origDigits.split('').sort().join('');
+  prodDigits = prodDigits.split('').sort().join('');
+
+  return prodDigits === origDigits ? true : false;
+}
